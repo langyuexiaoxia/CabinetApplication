7b9660441c81b9bcaf6e3a0d0a410602b79f525e
wangyue <1838419577@qq.com>
2018-06-15 16:25:19 +0800

    框架搭建

---
+++ app/src/main/java/com/wy/cabinet/activity/PayActivity.java
@@ -0,0 +1,21 @@
+package com.wy.cabinet.activity;
+
+import com.wy.cabinet.R;
+
+/**
+ * Created by Administrator on 2018/6/15.
+ * 支付方式选择界面
+ */
+
+public class PayActivity extends BaseActivity{
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_pay;
+    }
+
+    @Override
+    public void intData() {
+
+    }
+}
